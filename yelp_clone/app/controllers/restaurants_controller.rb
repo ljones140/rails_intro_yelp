10aad1307ecfc4b15e52df6f38a3ad8733ca4350
Emily Carson <emilyjanecarson@gmail.com>
2015-09-02 11:12:29 +0100

    can edit and delete restaurants

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -5,11 +5,33 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
-    Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to restaurants_path
   end
 
   def restaurant_params
